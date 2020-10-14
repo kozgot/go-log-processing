ce1079bfd1655d425c79dfc9dd0aa2e63fb0ea16
Kozák Ágota Boglárka <gogota97@gmail.com>
2020-10-14 20:13:33 +0200

    Filter by log level

--- cmd/logimport/main.go
+++ cmd/logimport/main.go
@@ -6,29 +6,41 @@ import (
 	"fmt"
 	"os"
 	"regexp"
-	"strings"
+	"time"
 )
+
+const logLevelsRegex = "(INFO|WARN)"
+const dateFormatRegex = "^(Mon|Tue|Wed|Thu|Fri|Sat|Sun) (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) (0?[1-9]|[12][0-9]|3[01]) ([0-1][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9] [0-2][0-9][0-9][0-9]"
+const dateLayoutString = "Mon Jan  2 15:04:05 2006"
 
 func main() {
 	filePath := "dc_main.log"
-	fmt.Println("Reading from" + filePath)
-
 	file, ferr := os.Open(filePath)
 	if ferr != nil {
 		panic(ferr)
 	}
 
-	r, _ := regexp.Compile("^(Mon|Tue|Wed|Thu|Fri|Sat|Sun) (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) (0?[1-9]|[12][0-9]|3[01]) ([0-1][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9] [0-2][0-9][0-9][0-9]")
-	ctr := 1
+	r, _ := regexp.Compile(dateFormatRegex)
+	levelRegex, _ := regexp.Compile(logLevelsRegex)
+
 	scanner := bufio.NewScanner(file)
+	relevantLines := []string{""}
 	for scanner.Scan() {
 		line := scanner.Text()
-		if strings.Contains(line, ":") {
-			date := r.FindString(line)
-			// items := strings.Split(line, ":")
-			// firstPart := strings.Join(items[0:3], ":")
-			fmt.Printf("Line %d: %s\n", ctr, date)
+		dateString := r.FindString(line)
+		if dateString != "" {
+
+			date, err := time.Parse(dateLayoutString, dateString)
+			if err != nil {
+				panic(err)
+			}
+
+			logLevel := levelRegex.FindString(line)
+			if logLevel != "" {
+				relevantLines = append(relevantLines, line)
+				fmt.Printf("Date %s: %s\n", date, logLevel)
+			}
 		}
-		ctr++
 	}
+	fmt.Println(len(relevantLines))
 }
