41f0206895f377dc2b3e43df21f17c525d7dce27
kozgot <37455584+kozgot@users.noreply.github.com>
2020-11-19 09:00:38 +0100

    complete rabbitMQ tutorial (#11)
    Co-authored-by: Kozák Ágota <kozak.agota@mirango.hu>

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -5,17 +5,27 @@ import (
 	"bufio"
 	"log"
 	"os"
+	"time"
 
 	elasticuploader "github.com/kozgot/go-log-processing/cmd/elasticsearch"
 	filter "github.com/kozgot/go-log-processing/cmd/filterlines"
 	contentparser "github.com/kozgot/go-log-processing/cmd/parsecontents"
 	"github.com/kozgot/go-log-processing/cmd/parsedates"
+	"github.com/streadway/amqp"
 )
 
 func main() {
 	// expects the file path from a command line argument (only works for dc_main.log files for now)
 	if len(os.Args) < 2 {
-		log.Fatalf("ERROR: Missing log file path param!")
+		/////////////////////////////////////////////////
+		// temporary rabbit	MQ tutorial code
+		for {
+			sendHello()
+			time.Sleep(1 * time.Second)
+		}
+		///////////////////////////////////////////////////////
+
+		//log.Fatalf("ERROR: Missing log file path param!")
 	}
 
 	filePath := os.Args[1]
@@ -43,4 +53,45 @@ func main() {
 	}
 
 	elasticuploader.BulkIndexerUpload(relevantLines)
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func sendHello() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	// create the channel
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	// queue
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	body := "Hello World!"
+	err = ch.Publish(
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	log.Printf(" [x] Sent %s", body)
+	failOnError(err, "Failed to publish a message")
 }
