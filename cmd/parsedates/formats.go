b6bbc532ee30c39c3a278f15943939570096913f
Kozák Ágota <kozak.agota@mirango.hu>
2021-03-01 20:24:01 +0100

    Remove log level regex from date parser constants

--- cmd/parsedates/formats.go
+++ cmd/parsedates/formats.go
@@ -1,7 +1,4 @@
 package parsedates
 
-// LogLevelsRegex represents the regular expression that matches the log level in a line of the dc_main.log file
-const LogLevelsRegex = "(INFO|WARN)"
-
 // DateFormatRegex represents the regular expression that matches the timestamp in a line of the dc_main.log file
 const DateFormatRegex = "^(Mon|Tue|Wed|Thu|Fri|Sat|Sun) (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) (0?[1-9]|[12][0-9]|3[01]) ([0-1][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9] [0-2][0-9][0-9][0-9]"
