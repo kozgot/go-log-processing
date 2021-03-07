cd5995b7b80c0a4102c48f54dd086a7a71cf704e
kozgot <gogota97@gmail.com>
2021-03-07 18:59:27 +0100

    Parse the new date format

--- cmd/parsedates/layoutstrings.go
+++ cmd/parsedates/layoutstrings.go
@@ -2,3 +2,6 @@ package parsedates
 
 // DateLayoutString represents the layout string for the timestamps in the dc_main.log file
 const DateLayoutString = "Mon Jan  2 15:04:05 2006"
+
+// DateLayoutStringShort represents the layout string for the timestamps in the plc_manager.log file
+const DateLayoutStringShort = "2006-01-02-15:04:05"
