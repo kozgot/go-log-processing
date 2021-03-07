eb6a0528637af81fdcff5962c28864862891b433
kozgot <gogota97@gmail.com>
2021-03-07 18:56:21 +0100

    Match INFO log levels

--- cmd/filterlines/formats.go
+++ cmd/filterlines/formats.go
@@ -1,4 +1,4 @@
 package filterlines
 
 // LogLevelsRegex represents the regular expression that matches the log level in a line of the dc_main.log file
-const LogLevelsRegex = "(ERROR|WARNING|WARN)"
+const LogLevelsRegex = "(ERROR|WARNING|WARN|INFO)"
