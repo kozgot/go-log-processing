13fc236e4869d39267704a66c7ceca5513ba77f0
Kozák Ágota <kozak.agota@mirango.hu>
2021-03-09 00:26:01 +0100

    Add file for dc info entry parsing WIP

---
+++ cmd/parsecontents/dcMessageParserLogic.go
@@ -0,0 +1,10 @@
+package parsecontents
+
+func parseDCMessage(lin string) *DCMessageParams {
+	dcMessageParams := DCMessageParams{}
+
+	// todo: implementation
+	// from dc_main: message sent/recieved lines with all kinds of payload (for now, that is enough)
+
+	return &dcMessageParams
+}
